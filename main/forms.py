ae8d67a9f1a7d327abc2530cb811672885038f95
zakutnya <yazakutny@gmail.com>
2017-11-08 11:12:20 +0300

    added datepicker to form, styled order stages

--- main/forms.py
+++ main/forms.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from django import forms
 
 from .models import Order
@@ -7,4 +9,10 @@ class OrderForm(forms.ModelForm):
 	
 	class Meta:
 		model = Order
-		fields = ('name', 'tel', 'delivery_date')
+		widgets = {
+			'name': forms.TextInput(attrs={'placeholder': u'Имя*', 'required': True}),
+			'tel': forms.TextInput(attrs={'placeholder': u'Телефон*', 'required': True}),
+			'delivery_date': forms.TextInput(attrs={'required':True}),
+			'order': forms.TextInput(attrs={'type':'hidden', 'required':True}),
+		}
+		exclude = ()
